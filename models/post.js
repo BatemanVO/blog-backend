267b805ac641a0596d1e87ee175baaa95e440b16
batemanvo <batemanvo@gmail.com>
2020-10-02 14:01:56 -0700

    Creating a model for blog messages.

--- models/post.js
+++ models/post.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+module.exports = {
+    Message: mongoose.model('Message', new mongoose.Schema({
+        title: String,
+        body: String,
+        date: { type: Date, default: Date.now }
+    }))
+};
