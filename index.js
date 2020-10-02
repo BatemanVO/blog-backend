b88639d76f19e561b6d2ad60530d3ec26d164347
batemanvo <batemanvo@gmail.com>
2020-10-02 14:25:26 -0700

    Creating an endpoint to retrieve all Posts in the database.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const app = require('express')();
+const { Post } = require('./models/post.js');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(process.env.PORT));
+
+app.get('/posts', (request, response) => {
+    Post.find({}).then(posts => response.json(posts));
+});
