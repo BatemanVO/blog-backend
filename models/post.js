5663946e8da8465343164adf040ea57ea8d47c2d
batemanvo <batemanvo@gmail.com>
2020-10-02 14:04:36 -0700

    Adding timestamps to posts instead of accepting a Date.

--- models/post.js
+++ models/post.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 module.exports = {
-    Message: mongoose.model('Message', new mongoose.Schema({
-        title: String,
-        body: String,
-        date: { type: Date, default: Date.now }
-    }))
+    Message: mongoose.model('Message', new mongoose.Schema(
+        {
+            title: String,
+            body: String
+        },
+        {
+            timestamps: {
+                createdAt: 'createdDate',
+                updatedAt: 'modifiedDate'
+            }
+        })
+    )
 };
